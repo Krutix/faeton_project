8424e46ecdf46457d8cecded8442668485a6799e
Andrei Krutikhin <pigeon.krutix@gmail.com>
2025-05-30 22:06:54 +0300

    Add basic movement for character template

---
+++ scripts/player_character.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+var speed = 1000
+var direction: Vector2 = Vector2(0,0)
+
+func _physics_process(delta: float) -> void:
+	player_movement(delta)
+
+func player_movement(delta: float) -> void:
+	direction = Vector2(0, 0)
+
+	if Input.is_action_pressed("ui_right"):
+		direction.x += 1
+	if Input.is_action_pressed("ui_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("ui_up"):
+		direction.y -= 1
+	if Input.is_action_pressed("ui_down"):
+		direction.y += 1
+
+	velocity = direction.normalized() * speed * delta
+
+	move_and_slide()
